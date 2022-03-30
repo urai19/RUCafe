09002d1220319db77bbdc48e3c0e5aecd27dd7c5
garvitgupta <garvitgupta3000@gmail.com>
2022-03-30 19:01:53 -0400

    Finish Order And Store Order

--- src/main/java/rucafe/project4/Order.java
+++ src/main/java/rucafe/project4/Order.java
@@ -2,6 +2,48 @@ package rucafe.project4;
 
 import java.util.ArrayList;
 
-public class Order {
+
+public class Order implements Customizable {
+
     private ArrayList<MenuItem> itemList;
+    private long orderNumber;
+    private int numItems;
+    private double orderCost;
+
+    public Order(long uniqueNumber) {
+        this.itemList = new ArrayList<>();
+        this.orderNumber = uniqueNumber;
+        this.numItems = 0;
+        this.orderCost = 0;
+    }
+
+    public long getOrderNumber(){
+        return this.orderNumber;
+    }
+
+    @Override
+    public boolean add(Object obj) {
+        if (obj instanceof MenuItem){
+            MenuItem item = (MenuItem) obj;
+            itemList.add(item);
+            numItems++;
+            orderCost += item.itemPrice();
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean remove(Object obj) {
+        if (numItems > 0) {
+            if (obj instanceof MenuItem) {
+                MenuItem item = (MenuItem) obj;
+                itemList.remove(item);
+                numItems--;
+                orderCost -= item.itemPrice();
+                return true;
+            }
+        }
+        return false;
+    }
 }
--- src/main/java/rucafe/project4/StoreOrders.java
+++ src/main/java/rucafe/project4/StoreOrders.java
@@ -1,4 +1,39 @@
 package rucafe.project4;
 
-public class StoreOrders {
+import java.util.ArrayList;
+
+public class StoreOrders implements Customizable {
+
+    private ArrayList<Order> orderList;
+    private int numOrders;
+
+    public StoreOrders(){
+        this.orderList = new ArrayList<>();
+        numOrders = 0;
+
+    }
+
+    @Override
+    public boolean add(Object obj) {
+        if (obj instanceof Order){
+            Order item = (Order) obj;
+            orderList.add(item);
+            numOrders++;
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean remove(Object obj) {
+        if (numOrders > 0) {
+            if (obj instanceof Order) {
+                Order item = (Order) obj;
+                orderList.remove(item);
+                numOrders--;
+                return true;
+            }
+        }
+        return false;
+    }
 }
